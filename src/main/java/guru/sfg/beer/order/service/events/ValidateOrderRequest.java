a0d0b13daec2d195dd0643d9605f75fcb48534e8
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2020-02-04 23:52:47 -0300

    Send validate order request with action. Closes #9

---
+++ src/main/java/guru/sfg/beer/order/service/events/ValidateOrderRequest.java
@@ -0,0 +1,17 @@
+package guru.sfg.beer.order.service.events;
+
+import guru.sfg.beer.order.service.web.model.BeerOrderDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ValidateOrderRequest {
+
+    private BeerOrderDto beerOrderDto;
+
+}
